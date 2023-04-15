9c13f39e6af9bde4a4ede24593dd03267f19a743
rkibistu <ogreanrazvan@gmail.com>
2023-04-15 10:22:56 +0300

    empty template. Packages installed

---
+++ Tests/TestSetup.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Fusion;
+using Fusion.KCC;
+
+public class TestSetup : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
